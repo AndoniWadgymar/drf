e956160e1a6f62bd84c67025c1293891d01a3867
Andoni Wadgymar <andowii@gmail.com>
2023-12-05 10:45:44 -0600

    Ingest Data with DRF

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -6,15 +6,28 @@ from rest_framework.decorators import api_view
 from products.models import Product
 from products.serializers import ProductSerializer
 
-@api_view(["GET"])
+# @api_view(["GET"])
+# def api_home(request, *args, **kwargs):
+#   """
+#   DRF API VIEW
+#   """
+#   instance = Product.objects.all().order_by("?").first()
+#   data = {}
+#   if instance:
+#     # data = model_to_dict(model_data, fields=['id', 'title', 'price']) #clean and easy conversion
+#     data = ProductSerializer(instance).data
+
+#   return Response(data)
+
+@api_view(["POST"])
 def api_home(request, *args, **kwargs):
   """
   DRF API VIEW
   """
-  instance = Product.objects.all().order_by("?").first()
-  data = {}
-  if instance:
-    # data = model_to_dict(model_data, fields=['id', 'title', 'price']) #clean and easy conversion
-    data = ProductSerializer(instance).data
-
-  return Response(data)
+  serializer = ProductSerializer(data=request.data)
+  if serializer.is_valid(raise_exception=True):
+    # instance = serializer.save()
+    print(serializer.data)
+    data = serializer.data
+    return Response(data)
+  return Response({"invalid": "Not good data"}, status=400)
--- py_client/basic.py
+++ py_client/basic.py
@@ -1,9 +1,15 @@
 import requests
 
-endpoint = "http://localhost:8000/api"
+endpoint = "http://localhost:8000/api/"
 
-get_response = requests.get(endpoint, params={"abc":123}, json={"name":"andoni"}) #Application programming interface
+# get_response = requests.get(endpoint, params={"abc":123}, json={"name":"andoni"}) #Application programming interface
 
-print(get_response.text) #print raw response
-print(get_response.status_code)
-print(get_response.json())
+# print(get_response.text) #print raw response
+# print(get_response.status_code)
+# print(get_response.json())
+
+post_response = requests.post(endpoint, json={"title": None, 'content':'Post TEST'})
+
+# print(post_response.text) #print raw response
+# print(post_response.status_code)
+# print(post_response.json())
