6d9df82e80b3e7e01c76b098356246830246c6dc
Andoni Wadgymar <andowii@gmail.com>
2023-12-05 09:41:03 -0600

    Rest Framework View & Response

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -1,18 +1,18 @@
-from django.http import JsonResponse
-import json
 from django.forms.models import model_to_dict
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from products.models import Product
 
-# Create your views here.
+@api_view(["GET"])
 def api_home(request, *args, **kwargs):
+  """
+  DRF API VIEW
+  """
   model_data = Product.objects.all().order_by("?").first()
   data = {}
   if model_data:
-    # data['id'] = model_data.id
-    # data['title'] = model_data.title
-    # data['content'] = model_data.content
-    # data['price'] = model_data.price
-    data = model_to_dict(model_data, fields=['id', 'price'])
+    data = model_to_dict(model_data, fields=['id', 'title', 'price']) #clean and easy conversion
 
-  return JsonResponse(data)
+  return Response(data)
