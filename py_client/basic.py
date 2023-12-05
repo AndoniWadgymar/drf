b2fc70e0aaeefcbd110397e0b95cb555fc9f2ec6
Andoni Wadgymar <andowii@gmail.com>
2023-12-04 21:24:53 -0600

    Echo GET data

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,8 @@ import requests
 
 endpoint = "http://localhost:8000/api"
 
-get_response = requests.get(endpoint) #Application programming interface
+get_response = requests.get(endpoint, params={"abc":123}, json={"name":"andoni"}) #Application programming interface
+
 print(get_response.text) #print raw response
 print(get_response.status_code)
 print(get_response.json())
