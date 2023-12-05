9cf2c2cfe5daeeeabd58736eb9dc9c3fda8a4285
Andoni Wadgymar <andowii@gmail.com>
2023-12-04 21:10:56 -0600

    Add API app, create urls file, add urls to main and create base api view

--- py_client/basic.py
+++ py_client/basic.py
@@ -1,7 +1,8 @@
 import requests
 
-endpoint = "http://localhost:8000/"
+endpoint = "http://localhost:8000/api"
 
 get_response = requests.get(endpoint) #Application programming interface
 print(get_response.text) #print raw response
 print(get_response.status_code)
+print(get_response.json())
