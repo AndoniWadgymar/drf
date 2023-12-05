161266fe08ce97eba19fd7fa4b2c3389fd43fae7
Andoni Wadgymar <andowii@gmail.com>
2023-12-05 10:16:36 -0600

    Django Rest Framwork Model Serializer

--- backend/cfehome/api/views.py
+++ backend/cfehome/api/views.py
@@ -4,15 +4,17 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from products.models import Product
+from products.serializers import ProductSerializer
 
 @api_view(["GET"])
 def api_home(request, *args, **kwargs):
   """
   DRF API VIEW
   """
-  model_data = Product.objects.all().order_by("?").first()
+  instance = Product.objects.all().order_by("?").first()
   data = {}
-  if model_data:
-    data = model_to_dict(model_data, fields=['id', 'title', 'price']) #clean and easy conversion
+  if instance:
+    # data = model_to_dict(model_data, fields=['id', 'title', 'price']) #clean and easy conversion
+    data = ProductSerializer(instance).data
 
   return Response(data)
